1f92cf3f21bb436b8ccf3a024bdb6a7ce962866b
subham shrestha <subham.shrestha@vairav.net>
2024-05-13 15:50:49 +0545

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -5,19 +5,22 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    // socket.write('HTTP/1.1 200 OK\r\n\r\n')
     socket.on("data", (data) => {
         const parsedData = data.toString().split('\r\n')
         const [startLine, headers] = parsedData
+
         const path = startLine.split(' ').filter((data) => data.startsWith('/'))[0]
-        console.log("🚀 ~ socket.on ~ path:", path)
-        const statusMethod = startLine.split(' ')[0]
         const endpoint = path.split('/')[2]
-        if(statusMethod == 'GET' && endpoint){
+
+        if (path == '/user-agent') {
+            const content = parsedData[2].split(' ')[1]
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
+        }
+
+        const echo = path.split('/')[1]
+        if (echo == 'echo') {
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${endpoint.length}\r\n\r\n${endpoint}`)
         }
- 
-
 
         if (path == '/') {
             socket.write('HTTP/1.1 200 OK\r\n\r\n')
