c41375576b5cf2937f8cac7bc07c5703c91d7472
subham shrestha <subham.shrestha@vairav.net>
2024-05-13 14:52:24 +0545

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -5,11 +5,25 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.write('HTTP/1.1 200 OK\r\n\r\n')
-  socket.on("close", () => {
-    socket.end();
-    server.close();
-  });
+    // socket.write('HTTP/1.1 200 OK\r\n\r\n')
+    socket.on("data", (data) => {
+        const parsedData = data.toString().split('\r\n')
+        const [startLine, headers] = parsedData
+        const path = startLine.split(' ').filter((data) => data.startsWith('/'))[0]
+
+        
+        if (path == '/') {
+            socket.write('HTTP/1.1 200 OK\r\n\r\n')
+        }
+        else {
+            socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+
+        }
+    })
+    socket.on("close", () => {
+        socket.end();
+        server.close();
+    });
 });
 
 server.listen(4221, "localhost");
